368edc237e9a63225c380fc650e53ea5471d1076
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-20 17:33:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def main(client, message):
     krakenapi = requests.get(url="https://krakenfiles.com/api/server/available").json()
     krakenxurl = krakenapi["data"]["url"]
     krakentoken = krakenapi["data"]["serverAccessToken"]
-    krakenupload = requests.post(url=krakenxurlurl, files={"upload_file": open(fukpath, "rb")}, serverAccessToken=krakentoken).json()
+    krakenupload = requests.post(url=krakenxurl, files={"upload_file": open(fukpath, "rb")}, serverAccessToken=krakentoken).json()
     krakenlink = krakenupload["data"]["url"]
     await status.delete()
     os.remove(file)
